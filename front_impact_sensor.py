88eab18c60be986f9cf48979e6bf67d3042aef81
ReDDsMediaSam <samdowrick@gmail.com>
2015-07-29 17:22:31 +0100

    front impact

--- front_impact_sensor.py
+++ front_impact_sensor.py
@@ -7,6 +7,11 @@ def setup_front_sensor ():
 def arm_movement ():
     pin3 = R.ruggeduinos[0].digital_read(buttonPin)
     if pin3:
-        R.servos["0WW24"][1] = -40
-    else:
-        R.servos["0WW24"][1] = 0
+        #R.servos["0WW24"][1] = -40
+        print "success"
+    #else:
+        #R.servos["0WW24"][1] = 0
+
+setup_front_sensor()
+while True:
+    arm_movement()
